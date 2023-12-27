480a54b23f44993699f3b70a887208d0664ee78e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-12-27 09:41:22 +0000

    added 2 new custom func

---
+++ functions/v1/randomMangaID.js
@@ -0,0 +1,28 @@
+const { JIKAN_CLIENT } = require('../../func/jikan.js');
+const error = require('../../utils/error.js');
+
+module.exports = {
+  name: "$randomMangaID",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err); 
+
+    let [sfw] = data.inside.splits;
+
+    if (sfw != "true" && sfw != "false") return error.newError(d, "Invalid Boolean in sfw option");
+
+    sfw = (sfw === "true")
+
+    try {
+      const random = await JIKAN_CLIENT.manga.random(sfw)
+      data.result = random.id;
+      return {
+        code: d.util.setCode(data)
+      };     
+    } catch (e) {
+      console.log(e);
+      error.newError(d, e);      
+    }
+  }
+}
